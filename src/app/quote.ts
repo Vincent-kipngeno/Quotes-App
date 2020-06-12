a345e5a83d91b767f2e5723056ce23468c14b16b
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 14:58:51 +0300

    Added a date property to class constructor and initialized its values in quoteComponent

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showQuoteDetails: boolean;
-  constructor (public quoteValue: string, public authorName: string, public userName: string, public upVote: number, public downVote: number) {
+  constructor (public quoteValue: string, public authorName: string, public userName: string, public upVote: number, public downVote: number, public date: Date) {
     this.showQuoteDetails = false;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote ("Be yourself; everyone else is already taken.", "Oscar Wilde", "Vincent Kipngeno", 0, 0),
-    new Quote ("Be the change that you wish to see in the world.", " Mahatma Gandhi", "Vincent Kipngeno", 0, 0),
-    new Quote ("No one can make you feel inferior without your consent.", "Eleanor Roosevelt", "Vincent Kipngeno", 0, 0),
-    new Quote ("Live as if you were to die tomorrow. Learn as if you were to live forever.", "Mahatma Gandhi", "Vincent Kipngeno", 0, 0)
+    new Quote ("Be yourself; everyone else is already taken.", "Oscar Wilde", "Vincent Kipngeno", 0, 0, new Date(2020, 3, 12)),
+    new Quote ("Be the change that you wish to see in the world.", " Mahatma Gandhi", "Vincent Kipngeno", 0, 0, new Date(2020, 3, 12)),
+    new Quote ("No one can make you feel inferior without your consent.", "Eleanor Roosevelt", "Vincent Kipngeno", 0, 0, new Date(2020, 3, 12)),
+    new Quote ("Live as if you were to die tomorrow. Learn as if you were to live forever.", "Mahatma Gandhi", "Vincent Kipngeno", 0, 0, new Date(2020, 3, 12))
   ]
   toggler(index){
     this.quotes[index].showQuoteDetails = !this.quotes[index].showQuoteDetails;
