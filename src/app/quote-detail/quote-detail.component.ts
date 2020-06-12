4f47221d534c3d32a28b05790d1329e00892a307
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 14:07:51 +0300

    import input and Quote class to quote-detail.component and perform input binding on quote-detail child component to display quote details

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
-
+  @Input() quote: Quote;
   constructor() { }
 
   ngOnInit(): void {
