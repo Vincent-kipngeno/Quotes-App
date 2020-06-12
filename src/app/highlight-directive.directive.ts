df9ffd5d6225235e7016406d4bd52e76a38f7c7c
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 21:51:39 +0300

    add input binding to highlight directive and added an attribute directive that highlights the quote

--- src/app/highlight-directive.directive.ts
+++ src/app/highlight-directive.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightDirective]'
 })
 export class HighlightDirectiveDirective {
+  @Input('appHighlightDirective') highlightColor: string;
+  constructor( private elem: ElementRef, ) { }
 
-  constructor() { }
+  highlight(color: string){
+    color = this.highlightColor;
+    this.elem.nativeElement.style.backgroundColor= color;
+  }
 
 }
