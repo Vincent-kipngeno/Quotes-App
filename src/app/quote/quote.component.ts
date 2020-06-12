4af2dc433d1d6b2d29d18d9d44b7c9215cbf2cba
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 13:40:07 +0300

    assigned values to quotes object array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,12 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote ("Be yourself; everyone else is already taken.", "Oscar Wilde", "Vincent Kipngeno", 0, 0),
+    new Quote ("Be the change that you wish to see in the world.", " Mahatma Gandhi", "Vincent Kipngeno", 0, 0),
+    new Quote ("No one can make you feel inferior without your consent.", "Eleanor Roosevelt", "Vincent Kipngeno", 0, 0),
+    new Quote ("Live as if you were to die tomorrow. Learn as if you were to live forever.", "Mahatma Gandhi", "Vincent Kipngeno", 0, 0)
+  ]
   constructor() { }
 
   ngOnInit(): void {
