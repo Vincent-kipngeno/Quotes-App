436a43d5e6332dc5eac47c07f4f0d1b9733d0e48
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 18:32:18 +0300

    perform output binding on quote form component and added submitGoal function that receives input from form input and emit event to parent component to be added to the array

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote'
 
 @Component({
@@ -6,7 +6,20 @@ import { Quote } from '../quote'
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
+
+
+
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote (" ", " ", " ", 0, 0, new Date());
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    alert("Your Quote has been added. You can also add another one.");
+    this.newQuote = new Quote (" ", " ", " ", 0, 0, new Date());
+  }
 
   constructor() { }
 
