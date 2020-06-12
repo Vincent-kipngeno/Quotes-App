1f8b7cb99105430e4c90ab369388305b7e230cd0
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-06-12 13:37:14 +0300

    generate quote component and import Quote class in quote.component.ts

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
